86e98be318a58dff51be738bb96b6dcc1f9acde1
Rob Monhemius <robmonhemius@gmail.com>
2021-09-23 17:10:14 +0100

    load-articles

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php 
 
+function theme_setup() {
+  add_theme_support( 'post-thumbnails' );
+}
+add_action('after_setup_theme', 'theme_setup');
+
 function theme_sidebars() {
   register_sidebar( [
     'id' => 'sidebar',
--- index.php
+++ index.php
@@ -10,16 +10,44 @@
       <div class="panel-body">
         <?php 
           if( have_posts() ): 
-            while( have_posts() ): the_post(); ?>
-              <div class="row">
-                <div class="col-md-3">IMAGE</div>
-                <div class="col-md-9">
-                  <?php the_title( 'h1', 'h1' ); ?>
+            while( have_posts() ): the_post();?>
+            <article class="post">
+              <?php if( has_post_thumbnail() ): ?>
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="post-thumbnail">
+                      <?php the_post_thumbnail(); ?>
+                    </div>
+                  </div>
+                  <div class="col-md-9">
+                    <a href="<?php the_permalink(); ?>">
+                      <?php the_title( '<h1>', '</h1>' ); ?>
+                    </a>
+                    <p class="meta">Posted by <b><?php the_author(); ?></b> at <?php the_date();?> on <?php the_time(); ?></p>
+                  <div class="excerpt">
+                    <?php the_excerpt(); ?>
+                  </div>
+                  <a href="<?php the_permalink(); ?>" class="btn btn-default">Read More &raquo;</a>
+                  </div>
                 </div>
-              </div>
+                <?php else: ?>
+                  <div class="row">
+                    <div class="col-md-12">
+                    <a href="<?php the_permalink(); ?>">
+                      <?php the_title( '<h1>', '</h1>' ); ?>
+                    </a>
+                    <p class="meta">Posted by <b><?php the_author(); ?></b> at <?php the_date();?> on <?php the_time(); ?></p>
+                    <div class="excerpt">
+                      <?php the_excerpt(); ?>
+                    </div>
+                    <a href="<?php the_permalink(); ?>" class="btn btn-default">Read More &raquo;</a>
+                    </div>
+                  </div>              
+                  <?php endif; ?>
+                </article>
             <?php endwhile;
           endif;
-        ?>
+          ?>
       </div>
     </div>
     <div class="col-md-4">
